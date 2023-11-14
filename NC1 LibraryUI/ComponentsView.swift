b0c043b21cbbbcaf7660028499b4cd7db44b2b00
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-11-14 13:13:40 +0100

    TitleBarView
    Created the view to display the title, centered in the HStack, independently from the button aligned to the trainling edge.

--- NC1 LibraryUI/ComponentsView.swift
+++ NC1 LibraryUI/ComponentsView.swift
@@ -4,13 +4,45 @@ import SwiftUI
 
 struct ComponentsView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            // Barra de título
+            TitleBarView()
+                .frame(height: 100)
+            // Área de contenido desplazable
+            ScrollView {
+                // Contenido de la ScrollView
+                ForEach(0...100, id: \.self) { index in
+                    Text("Elemento \(index)")
+                        .padding()
+                }
+            }
         }
-        .padding()
+        .padding(.horizontal)
+    }
+}
+
+/// Se encarga de mostrar un texto de título centrado perfectamente, en conjunto con un botón que se mantiene alineado a la derecha.
+struct TitleBarView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            HStack {
+                ZStack(alignment: .trailing) {
+                    Text("Controls")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .frame(width: geometry.size.width, alignment: .center)
+                    Button(action: {
+                        // Acción del botón
+                    }) {
+                        Image(systemName: "line.3.horizontal.decrease.circle")
+                    }
+                    .alignmentGuide(.trailing) { d in
+                        d[.trailing]
+                    }
+                }
+            }
+            .frame(height: 100)
+        }
     }
 }
 
--- NC1 LibraryUI/TabViewContainer.swift
+++ NC1 LibraryUI/TabViewContainer.swift
@@ -11,7 +11,7 @@ struct TabViewContainer: View {
                 }
             ComponentsView()
                 .tabItem {
-                    Label("Configuration", systemImage: "globe")
+                    Label("Configuration", systemImage: "gear")
                 }
         }
     }
