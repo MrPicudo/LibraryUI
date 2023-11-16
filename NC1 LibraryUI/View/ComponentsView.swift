e1274f0cb2faa9a07e0777ce2ee03f3c20692049
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-11-16 12:36:11 +0100

    Search function added
    Added the logic for the search function, separating the 3 sections of the list.

--- NC1 LibraryUI/View/ComponentsView.swift
+++ NC1 LibraryUI/View/ComponentsView.swift
@@ -3,10 +3,26 @@
 import SwiftUI
 
 struct ComponentsView: View {
-    
-    @State var searchText: String = ""
+    // Objeto que nos da acceso a los datos del modelo.
     var components = ComponentsData()
-    
+    // Objetos necesarios para realizar la búsqueda.
+    @State var searchText: String = ""
+    var filteredControls: [ComponentsModel] {
+        components.controlsData.filter {
+            $0.name.localizedCaseInsensitiveContains(searchText) || searchText.isEmpty
+        }
+    }
+    var filteredLayouts: [ComponentsModel] {
+        components.layoutsData.filter {
+            $0.name.localizedCaseInsensitiveContains(searchText) || searchText.isEmpty
+        }
+    }
+    var filteredMiscellaneous: [ComponentsModel] {
+        components.miscellaneousData.filter {
+            $0.name.localizedCaseInsensitiveContains(searchText) || searchText.isEmpty
+        }
+    }
+    // Iniciamos la variable que construye la vista
     var body: some View {
         ZStack {
             Color(.systemGray6)
@@ -33,35 +49,83 @@ struct ComponentsView: View {
                         
                         // Área de contenido desplazable
                         List {
-                            Group {
-                                // Contenido de la primera sección
-                                Section(header: Text("CONTROLS").font(.headline)) {
-                                    ForEach (components.controlsData) { component in
-                                        NavigationLink(destination: ButtonsView()) {
-                                            ListItemView(sfsymbol: component.image, component: component.name, color: component.color)
-                                        }
-                                    }
-                                }
-                                // Contenido de la segunda sección
-                                Section(header: Text("LAYOUTS").font(.headline)) {
-                                    ForEach (components.layoutsData) { component in
-                                        NavigationLink(destination: ButtonsView()) {
-                                            ListItemView(sfsymbol: component.image, component: component.name, color: component.color)
-                                        }
-                                    }
-                                }
-                                // Contenido de la tercera sección
-                                Section(header: Text("MISCELLANEOUS").font(.headline)) {
-                                    ForEach (components.miscellaneousData) { component in
-                                        NavigationLink(destination: ButtonsView()) {
-                                            ListItemView(sfsymbol: component.image, component: component.name, color: component.color)
-                                        }
-                                    }
-                                }
-                            }
                             
+                            if !searchText.isEmpty {
+                                // Sección de CONTROLS
+                                if !filteredControls.isEmpty {
+                                    Section(header: Text("CONTROLS").font(.headline)) {
+                                        ForEach(filteredControls) { component in
+                                            NavigationLink(destination: ButtonsView()) {
+                                                ListItemView(sfsymbol: component.image, component: component.name, color: component.color)
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    Section(header: Text("CONTROLS").font(.headline)) {
+                                        Text("No results found")
+                                    }
+                                }
+                                
+                                // Sección de LAYOUTS
+                                if !filteredLayouts.isEmpty {
+                                    Section(header: Text("LAYOUTS").font(.headline)) {
+                                        ForEach(filteredLayouts) { component in
+                                            NavigationLink(destination: ButtonsView()) {
+                                                ListItemView(sfsymbol: component.image, component: component.name, color: component.color)
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    Section(header: Text("LAYOUTS").font(.headline)) {
+                                        Text("No results found")
+                                    }
+                                }
+                                
+                                // Sección de MISCELLANEOUS
+                                if !filteredMiscellaneous.isEmpty {
+                                    Section(header: Text("MISCELLANEOUS").font(.headline)) {
+                                        ForEach(filteredMiscellaneous) { component in
+                                            NavigationLink(destination: ButtonsView()) {
+                                                ListItemView(sfsymbol: component.image, component: component.name, color: component.color)
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    Section(header: Text("MISCELLANEOUS").font(.headline)) {
+                                        Text("No results found")
+                                    }
+                                }
+                            } else {
+                                Group {
+                                    // Contenido de la primera sección
+                                    Section(header: Text("CONTROLS").font(.headline)) {
+                                        ForEach (components.controlsData) { component in
+                                            NavigationLink(destination: ButtonsView()) {
+                                                ListItemView(sfsymbol: component.image, component: component.name, color: component.color)
+                                            }
+                                        }
+                                    }
+                                    // Contenido de la segunda sección
+                                    Section(header: Text("LAYOUTS").font(.headline)) {
+                                        ForEach (components.layoutsData) { component in
+                                            NavigationLink(destination: ButtonsView()) {
+                                                ListItemView(sfsymbol: component.image, component: component.name, color: component.color)
+                                            }
+                                        }
+                                    }
+                                    // Contenido de la tercera sección
+                                    Section(header: Text("MISCELLANEOUS").font(.headline)) {
+                                        ForEach (components.miscellaneousData) { component in
+                                            NavigationLink(destination: ButtonsView()) {
+                                                ListItemView(sfsymbol: component.image, component: component.name, color: component.color)
+                                            }
+                                        }
+                                    }
+                                }
+                                
+                                
+                            }
                         }
-                                            
                     }
                 }
             }
